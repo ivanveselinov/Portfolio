9180827f09b0639dc006b8d07e186d11e8eebf89
Ivan <ivanveselinov22@gmail.com>
2021-12-02 18:19:18 +1100

    cv donenpm start

--- src/components/contact/ContactMain.js
+++ src/components/contact/ContactMain.js
@@ -14,13 +14,13 @@ function ContactMain() {
  
            {/* Contact form */}
     <div className=" w-screen h-20 bg-blue-100 flex justify-between items-center mt-40 ">
-      <a href="https://www.linkedin.com/in/ivanveselinov" className="lg:w-1/4 border bg-blue-100  text-2xl items-center  text-center p-5">
+      <a href="https://www.linkedin.com/in/ivanveselinov" className="lg:w-1/4 bg-blue-100  text-2xl items-center  text-center p-5">
         LinkedIn <LinkedInIcon sx ={{ fontSize: 40 }}/>
       </a>
-      <a href="{ cv }" download="Ivan%20Veselinov%20Resume.pdf" className="lg:w-1/4 border bg-blue-100 text-2xl text-center p-5">
+      <a href="https://github.com/ivanveselinov" className="lg:w-1/4 bg-blue-100 text-2xl text-center p-5">
        GitHub <GitHubIcon sx ={{ fontSize: 40 }}  />
       </a>
-      <a className="lg:w-1/4 border bg-blue-100 text-2xl text-center p-5 ">
+      <a href={process.env.PUBLIC_URL + '/cv/Ivan%20Veselinov%20Resume.pdf'} download="Ivan%20Veselinov%20Resume.pdf" className="lg:w-1/4 border bg-blue-100 text-2xl text-center p-5 ">
           CV <HistoryEduIcon sx ={{ fontSize: 40 }}/> 
       </a>
   
