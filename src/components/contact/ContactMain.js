dc7c2f750831ad6cecec951d9280ec708d445944
Ivan <ivanveselinov22@gmail.com>
2022-01-17 14:09:01 +1100

    minor changes on icon and _blank to foother

--- src/components/contact/ContactMain.js
+++ src/components/contact/ContactMain.js
@@ -13,10 +13,10 @@ function ContactMain() {
  
            {/* Contact form */}
     <div className=" w-screen h-20 bg-blue-100 flex justify-between items-center mt-40 ">
-      <a href="https://www.linkedin.com/in/ivanveselinov" className="lg:w-1/4 bg-blue-100  text-2xl items-center  text-center p-5">
+      <a href="https://www.linkedin.com/in/ivanveselinov" target="_blank" className="lg:w-1/4 bg-blue-100  text-2xl items-center  text-center p-5">
         LinkedIn <LinkedInIcon sx ={{ fontSize: 40 }} />
       </a>
-      <a href="https://github.com/ivanveselinov" className="lg:w-1/4 bg-blue-100 text-2xl text-center p-5">
+      <a href="https://github.com/ivanveselinov" target="_blank" className="lg:w-1/4 bg-blue-100 text-2xl text-center p-5">
        GitHub <GitHubIcon sx ={{ fontSize: 40 }}  />
       </a>
       <a href={process.env.PUBLIC_URL + '/cv/Ivan%20Veselinov%20Resume.pdf'} download="Ivan Veselinov Resume.pdf" className="lg:w-1/4 border bg-blue-100 text-2xl text-center p-5 ">
