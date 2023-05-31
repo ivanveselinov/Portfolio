5853ddeebe972a32b1ea8744ea97ae2ad4f1ec5f
Ivan <ivanveselinov22@gmail.com>
2023-05-31 19:50:47 +1000

    Minimised Home Font from 5xl to 4xl and also fixed indentation to workExperience bar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,18 +4,18 @@ import Typing from 'react-typing-animation';
 
 function Home() {
 
-    const text='sm:ml-1 lg:ml-5 p-2 font-bold ';
+    const text="lg:ml-5 sm:ml-1 p-2 font-bold";
 
     return (
         <div id="home" className="w-full mt-1 border-2 h-screen bg-home-lg bg-cover bg-center bg-repeat-y bg-fixed flex" >
            <div className=" mt-50 m-auto w-1/2 justify-center h-60 ">
-               <div className='sm:text-xl lg:text-5xl text-green-300 font-saira '>
+               <div className="lg:text-4xl md:text-3xl sm: text-2xl text-green-300 font-saira">
                     <Typing speed={100}>
-                        <p className='lg:p-2 font-bold '>Hello!</p> 
-                        <p className={` ${text} lg:ml-5`}>My name is Ivan Veselinov✌️</p> 
-                        <p className={`${text} lg:ml-10`}>I am a DevOps Engineer, Full-Stack Web Developer </p>
-                        <p className={`${text} lg:ml-16`}>and IT Technician 💻</p>
-                        <p className={`${text} lg:ml-24`}>based in Melbourne 🇦🇺</p>
+                        <p className="lg:p-2 font-bold">Hello!</p> 
+                        <p className={` ${text} lg:ml-5 md:ml-4 sm: ml-3`}>My name is Ivan Veselinov✌️</p> 
+                        <p className={`${text} lg:ml-10 md:ml-10 sm: ml-11 `}>I am a DevOps Engineer, Full-Stack Web Developer </p>
+                        <p className={`${text} lg:ml-16 md:ml-16 sm: ml-20`}>and IT Technician 💻</p>
+                        <p className={`${text} lg:ml-24 md:ml-24 sm: ml-28`}>based in Melbourne 🇦🇺</p>
                     <Typing.Speed ms={200} />
                         <p className='ml-5'></p>   
                     </Typing>
