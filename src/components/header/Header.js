71c0b22511f9d2ac1c471b2f7da99ce6ecd34e26
Ivan <ivanveselinov22@gmail.com>
2023-05-31 19:26:09 +1000

    Make CSS smaller in Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,21 +11,22 @@ import LaptopChromebookIcon from '@mui/icons-material/LaptopChromebook';
 
 
 function Header() {
+    const homeMenuBarSelection = "lg:text-xl hover:text-blue-300 hover:underline"
     const icons = "mb-2";
     return (
     <div className=" z-10 w-screen fixed top-0 bg-gradient-to-r from-blue-50 via-blue-100 to-blue-200 h-24 ">
         <div className="w-full h-20 justify-between flex sm: text-xs xl:text-base xs:text-center text-center sm: p-6 md:p-10 lg:p-10">
-            <AnchorLink href="#home" className="lg:text-xl hover:text-blue-300 hover:underline "><HomeIcon className={icons}/>Home</AnchorLink>
+            <AnchorLink href="#home" className={homeMenuBarSelection}><HomeIcon className={icons}/>Home</AnchorLink>
             <AnchorLink href="#about" className="lg:text-xl hover:text-blue-300 hover:underline  "><PersonIcon className={icons}/> About</AnchorLink>
             <AnchorLink href="#workExperience" className="lg:text-xl lg:flex md:flex sm:flex xs:block hover:text-blue-300 text-center hover:underline ">
                 <div><LaptopChromebookIcon className={`${icons} md:mt-0 lg:mt-1`}/></div>
                 <div className="xs:mt-2 sm:mt-2 md:mt-1 lg:mt-0"> Work Experience</div>
             </AnchorLink>
-            <AnchorLink href="#projects" className="lg:text-xl hover:text-blue-300 hover:underline"><CodeIcon className={icons}/>Projects</AnchorLink>
-            <AnchorLink href="#tech" className="lg:text-xl hover:text-blue-300 hover:underline"><WorkIcon className={icons}/>Skills</AnchorLink>
-            <AnchorLink href="#education" className="lg:text-xl hover:text-blue-300 hover:underline"><SchoolIcon className={icons}/>Education</AnchorLink>
-            <AnchorLink href="#publication" className="lg:text-xl hover:text-blue-300 hover:underline"><ArticleIcon className={icons}/>Publications</AnchorLink>
-            <AnchorLink href="#contact" className="lg:text-xl hover:text-blue-300 hover:underline"><ContactsIcon className='sm: mb-2 lg:mb-2.6'/>Contact</AnchorLink>
+            <AnchorLink href="#projects" className={homeMenuBarSelection}><CodeIcon className={icons}/>Projects</AnchorLink>
+            <AnchorLink href="#tech" className={homeMenuBarSelection}><WorkIcon className={icons}/>Skills</AnchorLink>
+            <AnchorLink href="#education" className={homeMenuBarSelection}><SchoolIcon className={icons}/>Education</AnchorLink>
+            <AnchorLink href="#publication" className={homeMenuBarSelection}><ArticleIcon className={icons}/>Publications</AnchorLink>
+            <AnchorLink href="#contact" className={homeMenuBarSelection}><ContactsIcon className='sm: mb-2 lg:mb-2.6'/>Contact</AnchorLink>
         </div>
      </div>
 
