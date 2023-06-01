d5e08fe5497218de3fbf264aff57f64b21f4a601
Ivan <ivanveselinov22@gmail.com>
2023-06-01 13:54:31 +1000

    More indentation done with Work Experience

--- src/components/workExperience/WorkExperience.js
+++ src/components/workExperience/WorkExperience.js
@@ -4,19 +4,20 @@ import { Fade, Zoom } from 'react-reveal'
 
 const WorkExperience = () => {
   
-  const FirstContainer="lg:flex md: block h-22 border-b" 
-  const Container="h-22 shadow-md bg-blue-200 mt-5 mb-2 items-center mr-1 ml-1 rounded-xl p-4 lg:full md: w-2/3 sm: w-full sm: m-auto"
-  const Main="w-full text-center text-2xl m-auto mt-8 "
-  const ContainerTwo="p-1 flex justijustify-between lg:w-3/4 md: w-3/4 sm:w-3/4 m-auto"
-  const Logo="bg-teamAssurance bg-no-repeat lg:w-24 lg:h-24 md:w-24 md:h-24 "
-  const Title="lg:text-3xl m-auto ml-10 border-b md: text-xl sm: text-2xl sm: text-center font-bold"
-  const positionTitle="text-center mt-1 w-full h-10 lg:text-xl font-bold md:text-l sm:text-l"
+  const FirstContainer="lg:flex md: block lg:w-full md: w-3/4 h-22 border-b" 
+  const Container="h-22 shadow-md bg-blue-200 mt-5 mb-2 items-center mr-1 ml-1 rounded-xl p-4 w-full sm: w-full sm: m-auto"
+  const Main="w-full text-center text-2xl m-auto mt-8"
+  const ContainerTwo="p-1 flex justijustify-between lg:w-4/5 m-auto md: w-3/4 sm: w-full border-2 "
+  const Logo="bg-no-repeat lg:w-24 lg:h-24 md:w-24 md:h-24 m-auto border-2" //Implement Hidden command here for Logo on sm screen like sm:hidden !!!
+  const Title="lg:text-3xl m-auto border-b md: text-xl sm: text-2xl font-bold m-auto border-2" //Title and logo need touch up for large screen !!
+  const positionTitle="text-center mt-1 w-full lg:h-10 lg:text-xl font-bold md:text-l sm:text-l sm: h-full"
   const since="text-center mt-1 w-full h-10  lg:text-xl font-bold md:text-l sm:text-l"
   const iconsTop = "mb-2"
   const workExperienceTitleMainContainer = "w-full m-auto h-40 flex"
-  const workExperienceTitleSecondContainer = "m-auto w-1/4 lg:flex md:flex md:w-auto sm: block sm: w-3/4 "
+  const workExperienceTitleSecondContainer = "m-auto w-1/4 lg:flex md:flex md:w-auto sm: block sm: w-3/4"
   const workExperienceTitleIconContainer = "w-12 h-12 m-auto" 
   const workExperienceTitleContainer = "w-full h-12 mt-2 text-2xl"
+
     return (
       <Fade duration={3000}> 
         <div className=" bg-gray-100 pt-4 pb-32">
@@ -60,6 +61,7 @@ const WorkExperience = () => {
                           <p className={`${Logo} bg-dekTechnologies`}></p>
                         </a>
                         <p className={Title}>Dek Technologies</p>
+                        <p className="bg-dekTechnologies"></p>
                         </div>
                         <p className={positionTitle}>Position/Title: DevOps Engineer</p>
                         <p className={since}>May 2023 - Present </p>
