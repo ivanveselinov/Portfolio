64f5651db59842d94dbb913d236452b6ed561085
Ivan <ivanveselinov22@gmail.com>
2024-04-14 00:17:38 +1000

    Added automatic work time duration

--- src/components/workExperience/WorkExperience.js
+++ src/components/workExperience/WorkExperience.js
@@ -1,64 +1,73 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import LaptopChromebookIcon from '@mui/icons-material/LaptopChromebook';
 import { Fade, Zoom } from 'react-reveal'
 
-function teamAssuranceEndDate(){
-  const TaStartDate = new Date('2022-01-15');
-  const TaEndDate = new Date('2023-02-15')
-  
-  let milisecounds = TaEndDate - TaStartDate
-  
-  let diffInDays = Math.floor(milisecounds / (1000 * 60 * 60 * 24));
-
-  if (diffInDays >= 365) {
-    let year = diffInDays / 365
-    let months = (diffInDays % 365) / 30;
-    console.log('Year: ', year)
-
-    if (months.toFixed(0) < 2){
-      console.log(`${year.toFixed(0)} year and ${months.toFixed(0)} month`)
-    } else{
-      console.log(`${year.toFixed(0)} year and ${months.toFixed(0)} months`)
-    }
-
-
-
-  }  
-
-}
-
-
-function CurrentWorkPlace(){
-
-  const currentDate = new Date();
-  const startDate = new Date('2023-04-30')
-  
-  let milisecounds = currentDate - startDate
-  
-  let diffInDays = Math.floor(milisecounds / (1000 * 60 * 60 * 24));
-
-  let year = diffInDays / 365
-  let months = (diffInDays % 365) / 30;
-  console.log('Year: ', year)
-
-    if (months.toFixed(0) < 2){
-      console.log(`${year.toFixed(0)} year and ${months.toFixed(0)} month`)
-    } else{
-      console.log(`${year.toFixed(0)} year and ${months.toFixed(0)} months`)
-    }
-
-    if (diffInDays < 365){
-      let  totalMonths = (diffInDays / 30) 
-      console.log(`${totalMonths.toFixed(0)} Months`)
-    }
-}
-
-CurrentWorkPlace()
-
-// teamAssuranceEndDate()
-
 const WorkExperience = () => {
-  
+
+  const [taYear, setTaYear] = useState('');
+  const [taMonth, setTaMonth] = useState('');
+
+  const [year, setYear] = useState('');
+  const [month, setMonth] = useState('');
+
+  useEffect(() => {
+
+    function teamAssuranceEndDate(){
+      const TaStartDate = new Date('2022-01-15');
+      const TaEndDate = new Date('2023-02-15')
+      
+      let milisecounds = TaEndDate - TaStartDate
+      
+      let diffInDays = Math.floor(milisecounds / (1000 * 60 * 60 * 24));
+    
+      if (diffInDays >= 365) {
+        let year = diffInDays / 365
+        let months = (diffInDays % 365) / 30;
+        console.log('Year: ', year)
+        console.log('taMonths', months)
+    
+        // if (months.toFixed(0) < 2){
+        //   console.log(`${year.toFixed(0)} year and ${months.toFixed(0)} month`)
+        // } else{
+        //   console.log(`${year.toFixed(0)} year and ${months.toFixed(0)} months`)
+        // }
+
+        setTaYear(year.toFixed(0));
+        setTaMonth(months.toFixed(0));
+      }  
+    }
+
+    function CurrentWorkPlace(){
+
+      const currentDate = new Date();
+      const startDate = new Date('2023-04-30')
+      
+      let milisecounds = currentDate - startDate
+      
+      let diffInDays = Math.floor(milisecounds / (1000 * 60 * 60 * 24));
+    
+      let year = diffInDays / 365
+      let months = (diffInDays % 365) / 30;
+      console.log('Year: ', year)
+    
+        if (diffInDays < 365){
+          let totalMonths = (diffInDays / 30) 
+          console.log(`${totalMonths.toFixed(0)} Months`)
+          setYear(0);
+          setMonth(months.toFixed(0));
+        } else{
+          let totalMonths = (diffInDays / 30) 
+          console.log(`${totalMonths.toFixed(0)} Months`)
+          setYear(year.toFixed(0));
+          setMonth(months.toFixed(0));
+        }
+    }
+
+    CurrentWorkPlace()
+    teamAssuranceEndDate()
+
+  }, [])
+
   const FirstContainer="lg:flex md: block lg:w-full md: w-3/4 h-22 border-b m-auto" 
   const Container="h-22 shadow-md bg-blue-200 mt-5 mb-2 items-center mr-1 ml-1 rounded-xl p-4 w-full sm: w-full sm: m-auto"
   const Main="w-full text-center text-2xl m-auto mt-8"
@@ -109,6 +118,7 @@ const WorkExperience = () => {
                       </div>
                       <p className={positionTitle}>Position/Title: DevOps Engineer</p>
                       <p className={since}>May 2023 - Present</p>
+                      <p className={since}>{year} years and {month} months</p>
                       </div>
                 </Zoom>  
                         {/* TeamAssurance */}    
@@ -125,7 +135,7 @@ const WorkExperience = () => {
                       </div>
                       <p className={positionTitle}>Position/Title: DevOps Developer</p>
                       <p className={since}>January 2022 - February 2023</p>
-                      <p className={since}>Joined on <span id="year"></span> year and <span id="month"></span> months</p>
+                      <p className={since}>{taYear} year and {taMonth} month</p>
                     </div>
                   </Zoom>    
                 </div>
